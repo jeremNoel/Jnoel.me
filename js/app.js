340f1be2fc1161f248c05ad5f7b0048fb3831d89
jeremie <pro_rish@yahoo.com>
2016-05-31 19:33:13 +0200

    first div finished, add some more features and typo

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    sectionsColor : ['#9E9E9E', '#EDF01D', '#201E21', '#9E9E9E'],
+    sectionsColor : ['#9E9E9E', '#F3EEBA', '#D1C79F', '#FFF8DC'],
     css3: true,
     scrollBar:true
   });
+
+jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+
+  
 });
